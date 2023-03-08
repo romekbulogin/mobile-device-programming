237e08fd0fbeda24934b8ded9a1e6031c39bb3ce
romekbulogin <roman.bulyg@gmail.com>
2023-03-08 21:23:42 +0300

    LabWork 4

--- app/src/main/java/ru/edu/myapplication/MainActivity.kt
+++ app/src/main/java/ru/edu/myapplication/MainActivity.kt
@@ -1,5 +1,12 @@
 package ru.edu.myapplication
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +16,11 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import ru.edu.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -28,9 +40,57 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val button = findViewById<Button>(R.id.button_auth)
+        createDialog()
+
+        createNotificationChannel()
+
+        var builder = NotificationCompat.Builder(this, "channelId")
+        builder.apply {
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+            setContentTitle("Test")
+            setContentText("Test message")
+            priority = NotificationCompat.PRIORITY_DEFAULT
+        }
+
+        with(NotificationManagerCompat.from(this)) {
+            if (ActivityCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            notify(1, builder.build())
+        }
+    }
+
+    override fun onCreatePanelMenu(featureId: Int, menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "channelId", "First channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = "Test desc"
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun createDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.apply {
+            setTitle("Warning")
+            setMessage("This app is test")
+            setPositiveButton("OK") { dialog, i ->
+
+            }
+            builder.show()
         }
     }
 
